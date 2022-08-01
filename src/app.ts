87dfafa30c4a40e2d7c650692179bb5de0620cb7
Diego Siqueira <diego.camomila@gmail.com>
2022-08-01 19:54:25 -0300

    feat: requisito 03, app  POST /user

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-// import { errorHandler } from './middlewares/errorHandler';
 import ProductRoute from './routes/ProductRoute';
+import UserRoute from './routes/UserRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', ProductRoute);
-
+app.use('/users', UserRoute);
 // app.use(errorHandler);
 
 export default app;
