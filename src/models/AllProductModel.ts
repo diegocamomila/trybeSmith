747f5f3385a178df258eabe64bc633521d9db315
Diego Siqueira <diego.camomila@gmail.com>
2022-07-31 21:10:30 -0300

    feat: requisito 02, models metodo get /product

---
+++ src/models/AllProductModel.ts
@@ -0,0 +1,11 @@
+import { RowDataPacket } from 'mysql2';
+import { ICreateProduct } from '../interfaces/product.interface';
+import connection from './connection';
+
+export default async (): Promise<ICreateProduct[]> => {
+  const query = 'SELECT id, name, amount, orderId FROM Trybesmith.Products';
+  const [result] = await connection.execute<RowDataPacket[]>(query);
+  // console.log(result);
+  
+  return result as ICreateProduct[];
+};
