c8f856b2468536ef37f4ba98ba979eb05ccfd7cb
Diego Siqueira <diego.camomila@gmail.com>
2022-07-31 21:11:33 -0300

    feat: requisito 02, controler metodo get /product

---
+++ src/controllers/AllProductController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import AllProductService from '../services/AllProductService';
+
+export default async (_req: Request, res: Response) => {
+  try {
+    const { status, allProduct } = await AllProductService();
+    // console.log(allProduct);
+    return res.status(status).json(allProduct);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
