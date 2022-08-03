e10ba86b39965f60a5e8222c6dd1c1643cf9023f
Diego Siqueira <diego.camomila@gmail.com>
2022-08-03 16:18:14 -0300

    feat: requisito 05, controler  metodo POST/login

---
+++ src/controllers/login/LoginController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { IUser } from '../../interfaces/user.interface';
+import LoginService from '../services/login/login.services';
+
+export default async (req: Request, res: Response) => {
+  try {
+    const user = req.body as IUser;
+    const token = await LoginService(user);
+    return res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
+};
